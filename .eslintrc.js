5325d0a31d1d693274c73ee98cbff5f8af1e2c50
AlexandreDev <eztalenda@gmail.com>
2023-03-05 16:20:19 -0300

    feat(core): Adding the eslint config and lib install.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,9 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
   rules: {
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-}
+};
